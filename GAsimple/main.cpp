3ca1eaf2a850304043457978370727f06a54347c
asithakuruppu <shamil.asitha@gmail.com>
2019-06-10 19:15:14 +0530

    Initial Commit

---
+++ GAsimple/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GAsimple
+//
+//  Created by Shamil Asitha Kuruppu on 6/10/19.
+//  Copyright © 2019 Ina via dahti sa. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
